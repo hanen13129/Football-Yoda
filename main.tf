6902ef00c2773a0865547dfdaaee082c9e29d120
“Hanen” <benhassenhanen@gmail.com>
2024-10-02 10:58:03 +0200

    update

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.21" 
+      version = "~> 3.21"
     }
   }
 }
 
 # Provider Block
 provider "aws" {
-  profile = "default" 
+  profile = "default"
   region  = "eu-central-1"
 }
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-00f07845aed8c0ee7" 
+  ami           = "ami-00f07845aed8c0ee7"
   instance_type = "t2.micro"
 }
